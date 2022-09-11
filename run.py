f27a23fbaca47d45d6ba783c192167ed04f57b13
nfepb <nicolas.planteb@gmail.com>
2022-09-11 14:37:06 +0000

    Adds comment for debug change upon submission

--- run.py
+++ run.py
@@ -4,5 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        # Change debug to False when submitting the project
+        debug=True)
